0fb1c3d4e33ee9b6064e355a16d671cc5952dbeb
elhalili <amine.elhalili1@gmail.com>
2024-07-04 03:24:00 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import { useState } from "react";
-import "./App.css";
+import { About, Column, } from "./components";
+import { about, resumeData, interests, contact } from "./data"
 
-function App() {
-  const [dark, setDark] = useState<boolean>(false);
-  const darkModeHandler = () => {
-    setDark(!dark);
-    document.body.classList.toggle("dark");
-  };
-
+const App = () => {
   return (
     <>
-      <button onClick={darkModeHandler}>
-        {dark && "Normal"}
-        {!dark && "Dark"}
-      </button>
-      <h1 className="text-sm font-bold dark:text-3xl">Hello world</h1>
+      <div className="blur"></div>
+      <main className="a-container">
+        <About name={about.name} occupation={about.occupation} about={about.about} />
+        <section className="scrollable">
+          <div className="main-grid">
+            {resumeData.map((e, i) => {
+              return <Column title={e.title} cells={e.cells} key={i} />
+            })}
+          </div>
+        </section>
+        <section className="story">
+          <h4>{interests.header}</h4>
+          {interests.content.map((e, i) => <p key={i}>{e}</p>)}
+        </section>
+        <section className="story">
+          <h3>{contact.header}</h3>
+          <p>
+            {contact.startWith}
+            {contact.contacts.map((e, i) => {
+              return (
+                <>
+                  <a key={i} className="link" target="_blank" href={e.link}>{e.content}</a>
+                  {(i < contact.contacts.length - 1) && " or "}
+                </>
+              );
+            })}
+            .
+          </p>
+        </section>
+      </main>
     </>
   );
 }
